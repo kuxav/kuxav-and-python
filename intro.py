2d3beafa75709a80f321dc3f982e812dda3d4ac2
Kusanalika Xamarin <112876441+kuxav@users.noreply.github.com>
2022-09-13 06:12:55 +0700

    Intro

---
+++ intro.py
@@ -0,0 +1,9 @@
+print("This is a cyber world. Hello. But still same and may be diff world because of spirit.");
+rest="EVEREST" ;
+collei="COLLEI" ;
+ia="IA" ;
+aether="AETHER" ;
+cj="CARL JOHNSON" ;
+kirito="KIRITO" ;
+print(rest , collei , ia , aether , cj , kirito);
+help(print);
