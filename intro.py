2840c89c02ae581db6c85d29c97188a384f13eff
Kusanalika Xamarin <112876441+kuxav@users.noreply.github.com>
2022-09-16 18:24:15 +0700

    Intro and Intro begin

--- intro.py
+++ intro.py
@@ -1,9 +1,18 @@
 print("This is a cyber world. Hello. But still same and may be diff world because of spirit.");
-rest="EVEREST" ;
-collei="COLLEI" ;
-ia="IA" ;
-aether="AETHER" ;
-cj="CARL JOHNSON" ;
-kirito="KIRITO" ;
-print(rest , collei , ia , aether , cj , kirito);
-help(print);
+rest="Everest PAW Patrol" ;
+collei="Collei Genshin" ;
+ia="IA Vocaloid" ;
+aether="Aether Genshin" ;
+cj="Carl Johnson" ;
+kirito="Kirito" ;
+print(rest , collei , ia , aether , cj , kirito); # Output variable result
+Amber=2 * 3 + 7 / 9 ** 2 // 1 % 2 ; # Operation in variable
+print(Amber); # Output Amber
+rck = rest + ", " + collei + ", and " + kirito ; # Gabungan variable sebelumnya // Combinate
+print(rck); # Result previous
+# This is comment from // Ini komentar dari github.com/kuxav
+# Setidaknya berhati-hati dalam penulisan komentar. Lihat skrip dan bisa memahami.
+# Bismillah dan hamdalah.
+# 
+# Kusanalika Xamarin (Kuxav)
+# ft. Kirito + Abyan, M. N. A.
---
+++ intro_.py
@@ -0,0 +1,5 @@
+# This is using print only and welcome to a technology code
+# But you can leave this and get spirit from 'ilahi'
+# Bismillah, begin.
+
+print("Hello, Kuxav and Nahida!")
